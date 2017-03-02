3db7b35ee2a804543e8f5ce1da860431347134d6
Andrew Faraday <andrewfaraday@hotmail.com>
2017-03-02 15:15:18 +0000

    Added back the obvious check

--- script/run.rb
+++ script/run.rb
@@ -3,11 +3,11 @@ require_relative '../lib/environment.rb'
 config = Config.from_files
 twitter_client = TwitterClient.new(config.twitter_config)
 
-#unless twitter_client.latest_tweet_done?
-generator = Generator.new(twitter_client.latest_tweet.text, config.templates)
-tweet_text = generator.run
-puts tweet_text
-twitter_client.send_tweet(tweet_text)
-#end
+unless twitter_client.latest_tweet_done?
+  generator = Generator.new(twitter_client.latest_tweet.text, config.templates)
+  tweet_text = generator.run
+  puts tweet_text
+  twitter_client.send_tweet(tweet_text)
+end
 
 
