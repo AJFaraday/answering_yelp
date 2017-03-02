9ebfcb52f95b76c5432eb8fdf508ed652ea998b9
Andrew Faraday <andrewfaraday@hotmail.com>
2017-03-02 17:34:37 +0000

    Added some logging stuffage

--- script/run.rb
+++ script/run.rb
@@ -3,7 +3,10 @@ require_relative '../lib/environment.rb'
 config = Config.from_files
 twitter_client = TwitterClient.new(config.twitter_config)
 
-unless twitter_client.latest_tweet_done?
+if twitter_client.latest_tweet_done?
+  puts 'tweet matches previous tweet'
+else
+  puts 'generating new tweet'
   generator = Generator.new(twitter_client.latest_tweet.text, config.templates)
   tweet_text = generator.run
   puts tweet_text
