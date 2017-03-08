57356aee7a56ef6e6581fd81538fc10effcac930
ajfaraday <andrewfaraday@hotamail.co.uk>
2017-03-08 17:14:03 +0000

    #2: Remove question marks from initial messages

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,8 +1,9 @@
 class Generator
 
-  def initialize(source, options)
+  def initialize(source, options, recipient='hard_to_yelp')
     @source = source
     @options = options
+    @recipient = recipient
     set_cursor
   end
 
@@ -14,12 +15,21 @@ class Generator
   def run
     template = self.template
     result = @source.gsub(/how hard is it to/i, template)
-    result = "@hard_to_yelp #{result}"
+    result = add_recipient(result)
+    result = fix_punctuation(result)
     result = run until result.length <= 140
     result
   end
 
   private
+
+  def add_recipient(message)
+    "@#{@recipient} #{message}"
+  end
+
+  def fix_punctuation(message)
+    message.gsub('?', '.')
+  end
 
   def set_cursor
     @cursor = rand(@options.count)
