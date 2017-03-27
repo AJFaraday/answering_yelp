2d986eae07896e093387f3b7e79ac022f7c659fc
Andrew Faraday <ajfaraday@edge-ka.com>
2017-03-27 20:55:07 +0100

    Added pretty darned pointless spec

--- spec/transforms/recipient_spec.rb
+++ spec/transforms/recipient_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 module Transforms
   describe Recipient do
 
-    let(:recipient) {'how_to_yelp'}
+    let(:arguments) {{recipient: 'how_to_yelp'}}
+
+    it 'should add the recipient to the start of anything' do
+      Recipient.run("It's pretty easy to juggle.", arguments)
+    end
 
   end
 end
